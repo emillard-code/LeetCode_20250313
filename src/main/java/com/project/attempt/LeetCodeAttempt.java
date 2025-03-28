ba6ac577f7336dfa4da8cee6d3e254295aa9220c
Emerson Millard <emillard.code@gmail.com>
2025-03-28 12:05:00 -0700

    Added challenge attempt.

--- src/main/java/com/project/attempt/LeetCodeAttempt.java
+++ src/main/java/com/project/attempt/LeetCodeAttempt.java
@@ -4,6 +4,49 @@ public class LeetCodeAttempt {
 
     public static void main(String[] args) {
 
+        int[] nums1 = { 2, 0, 2 };
+        int[][] queries1 = { { 0, 2, 1 }, { 0, 2, 1 }, { 1, 1, 3 } };
+        System.out.println(zeroArrayTransformationII(nums1, queries1));
+
+        int[] nums2 = { 4, 3, 2, 1 };
+        int[][] queries2 = { { 1, 3, 2 }, { 0 ,2, 1 } };
+        System.out.println(zeroArrayTransformationII(nums2, queries2));
+
+    }
+
+    public static int zeroArrayTransformationII(int[] nums, int[][] queries) {
+
+        int k = 0;
+
+        for (int i = 0; i < queries.length; i++) {
+
+            int x = queries[i][0];
+            int y = queries[i][1];
+            int value = queries[i][2];
+
+            for (int j = x; j <= y; j++) {
+
+                if (nums[j] >= value) {
+                    nums[j] = nums[j] - value;
+                } else if (nums[j] > 0) {
+                    nums[j] = 0;
+                }
+
+            }
+
+            k++;
+
+            for (int j = 0; j < nums.length; j++) {
+
+                if (nums[j] > 0) { break; }
+                if (j == nums.length - 1 && nums[j] == 0) { return k; }
+
+            }
+
+        }
+
+        return -1;
+
     }
 
 }
